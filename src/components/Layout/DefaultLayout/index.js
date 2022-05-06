f63a5fe5c8ab9d0dc87410938353bf356facf74f
khuongduy96cg <khuongduy96cg@gmail.com>
2022-05-06 12:03:11 +0700

    dung khung DefaultLayout

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,16 +1,20 @@
+import classNames from 'classnames/bind';
 import Header from '~/components/Layout/components/Header';
+import styles from './DefaultLayout.module.scss';
 import Sidebar from './Sidebar';
 
-function DefaultLayout({children}) {
-    return <div>
-        <Header />
-        <div className='container'>
-            <Sidebar />
-            <div className='content'>
-                {children}
+const cx = classNames.bind(styles);
+
+function DefaultLayout({ children }) {
+    return (
+        <div className={cx('wrapper')}>
+            <Header />
+            <div className={cx('container')}>
+                <Sidebar />
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
-    </div>;
+    );
 }
 
 export default DefaultLayout;
