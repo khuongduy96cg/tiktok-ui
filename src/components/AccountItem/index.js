24d834125fa440eea16aa902b13d1f7f2c3dc826
khuongduy96cg <khuongduy96cg@gmail.com>
2022-05-21 22:09:30 +0700

    custom-button

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,19 @@
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import classNames from "classnames/bind";
+import styles from './AccountItem.module.scss';
+const cx = classNames.bind(styles);
+function AccountItem() {
+    return <div className={cx('wrapper')}>
+            <img className={cx('avatar')} src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/95910bef0257937cf4a3d05184cc25c0.jpeg?x-expires=1652623200&x-signature=ThvgHX7C5hkhh%2F9GmdQFfah9eMQ%3D" alt="Hoaa" />
+            <div className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <span className={cx('username')}>Nguyenvana</span>
+            </div>
+    </div>
+}
+
+export default AccountItem;
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,65 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Tippy from '@tippyjs/react/headless';
+import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
+
+import Button from '~/components/Button';
+import { Wrapper as PropperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
+import images from '~/assets/image';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import AccountItem from '~/components/AccountItem';
 
 const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
+    const [searchResult, setSearchResult] = useState([]);
 
-        </div>
-    </header>
+    useEffect(() => {
+        setSearchResult([1,2,3])
+    },[])
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <img src={images.logo} alt="Tiktok" />
+
+                <Tippy
+                    visible={searchResult.length > 0}
+                    interactive
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                                <PropperWrapper>
+                                     <h4 className={cx('search-title')}>
+                                         Accounts
+                                     </h4>
+                                         <AccountItem />
+                                         <AccountItem />
+                                         <AccountItem />
+                                         <AccountItem />
+                                </PropperWrapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Seach accounts and videos" spellCheck={false} />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={cx('actions')}>
+                    <Button text>Upload</Button>
+                    <Button primary>Log in</Button>
+                </div>
+
+            </div>
+        </header>
+    );
 }
 
 export default Header;
