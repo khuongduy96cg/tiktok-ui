dac0891ad5984fea7b47d79fda151d6d15ac3fae
khuongduy96cg <khuongduy96.cg@gmail.com>
2022-05-29 10:47:36 +0700

    commit header ui

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -6,7 +6,8 @@ import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 const cx = classNames.bind(styles);
 function AccountItem({ data }) {
-    <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
+    return (
+        <Link to={`/@${data.nickname}`} className={cx('wrapper')}>
         <Image className={cx('avatar')} src={data.avatar} alt={data.full_name} />
         <div className={cx('info')}>
             <h4 className={cx('name')}>
@@ -15,7 +16,8 @@ function AccountItem({ data }) {
             </h4>
             <span className={cx('username')}>{data.nickname}</span>
         </div>
-    </Link>;
+    </Link>
+    );
 }
 
 export default AccountItem;
